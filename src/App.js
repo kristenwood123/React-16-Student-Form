d46dded4abb3bc633b769d01da2634fccef3a868
Kristen Woodward <you@example.com>
2021-01-25 17:11:47 -0800

    added Error Boundary

--- src/App.js
+++ src/App.js
@@ -1,34 +1,23 @@
 import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
-
+import ErrorBoundary from './components/ErrorBoundary'
 import StudentForm from './components/StudentForm';
 
 class App extends Component {
-
-  state = {
-    hasError: false
-  }
-
-  componentDidCath() {
-    this.setState({ hasError: true })
-  }
-  
   render() {
-    if (this.state.hasError) {
-      return <h1>Oh no! Something went wrong.</h1>
-    } else {
       return (
       <div className="App">
         <header className="App-header">
           <img src={logo} className="App-logo" alt="logo" />
           <h1 className="App-title">Welcome to React</h1>
         </header>
+        <ErrorBoundary>
         <StudentForm />
+        </ErrorBoundary>
       </div>
       ); 
     } 
   }
-}
 
 export default App;
